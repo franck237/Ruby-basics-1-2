796319957540e1638bbdc356a0e5b09d526f14ed
ekwayv8 <yvon.ekwalla@gmail.com>
2019-07-11 11:05:19 +0200

    ajout du fichier 01_pyramids.rb et mise à jour de readme.md

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,26 @@
+def half_pyramid
+	floor = "#"
+puts "Combien d'étages voulez vous dans votre pyramide?"
+print "> "
+number_floors = gets.to_i
+number_spaces = number_floors - 1
+lenght_spacing = " " * number_spaces 
+if number_floors > 25 
+	puts "La pyramide s'est effondrée, désolé"
+	exit 
+end
+puts "Voici votre pyramide"
+number_floors.times do 
+	puts lenght_spacing + floor
+	floor = floor + "#"
+	number_spaces = number_spaces - 1
+	lenght_spacing = " " * number_spaces
+	if number_spaces == 0
+		puts "#" * number_floors
+		exit
+	end
+end
+	
+end
+half_pyramid
+
